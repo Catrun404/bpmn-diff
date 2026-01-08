19ff364c2573d3ce98f86b50dd1697952341c652
GitHub Action <action@github.com>
2026-01-08 16:07:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/catrun404/bpmndiff/services/MyProjectService.kt
+++ src/main/kotlin/com/github/catrun404/bpmndiff/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.catrun404.bpmndiff.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.catrun404.bpmndiff.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/test/kotlin/com/github/catrun404/bpmndiff/MyPluginTest.kt
+++ src/test/kotlin/com/github/catrun404/bpmndiff/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.catrun404.bpmndiff
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.components.service
@@ -6,7 +6,7 @@ import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.catrun404.bpmndiff.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
